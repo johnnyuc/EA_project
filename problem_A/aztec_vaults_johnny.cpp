b28b3c9696cc806afe54fedf8d1eabcc5d24efe7
Johnny Fernandes <alfernandes@student.dei.uc.pt>
2024-03-11 19:49:20 +0000

    Update with locklines in place

--- problem_A/aztec_vaults_johnny.cpp
+++ problem_A/aztec_vaults_johnny.cpp
@@ -6,27 +6,29 @@
 #define RIGHT 1
 
 bool verbose = false;
+int count_locks = 0, count = 0;
 
 struct vault {
     int best = -1;
-    int max_moves{};
     int nr_rows{};
     int nr_columns{};
+    int max_moves{};
+
     std::vector<std::vector<int>> matrix;
 };
 
 struct vault read() {
-    struct vault vault;
-    std::cin >> vault.nr_rows >> vault.nr_columns >> vault.max_moves;
+    struct vault v;
+    std::cin >> v.nr_rows >> v.nr_columns >> v.max_moves;
 
-    std::vector<std::vector<int>> vault_matrix(vault.nr_rows, std::vector<int>(vault.nr_columns));
+    std::vector<std::vector<int>> v_matrix(v.nr_rows, std::vector<int>(v.nr_columns));
 
-    for (int i_row = 0; i_row < vault.nr_rows; i_row++)
-        for (int i_column = 0; i_column < vault.nr_columns; i_column++)
-            std::cin >> vault_matrix[i_row][i_column];
+    for (int i_row = 0; i_row < v.nr_rows; i_row++)
+        for (int i_column = 0; i_column < v.nr_columns; i_column++)
+            std::cin >> v_matrix[i_row][i_column];
 
-    vault.matrix = vault_matrix;
-    return vault;
+    v.matrix = v_matrix;
+    return v;
 }
 
 bool verify(const struct vault& v) {
@@ -39,6 +41,7 @@ bool verify(const struct vault& v) {
 }
 
 void rotate(struct vault& v, int x, int y, int side, bool undo) {
+    count++;
     int temp = v.matrix[y][x];
 
     // Rotate right
@@ -48,7 +51,7 @@ void rotate(struct vault& v, int x, int y, int side, bool undo) {
         v.matrix[y + 1][x + 1] = v.matrix[y][x + 1];
         v.matrix[y][x + 1] = temp;
     }
-        // Rotate left
+    // Rotate left
     else {
         v.matrix[y][x] = v.matrix[y][x + 1];
         v.matrix[y][x + 1] = v.matrix[y + 1][x + 1];
@@ -56,17 +59,15 @@ void rotate(struct vault& v, int x, int y, int side, bool undo) {
         v.matrix[y + 1][x] = temp;
     }
 
-    if (undo)
-        v.max_moves++;
-    else
-        v.max_moves--;
+    if (undo) v.max_moves++;
+    else v.max_moves--;
 }
 
 bool preprocess(const struct vault& v) {
     // 2x2 diagonal matrix check
     if (v.nr_rows == 2 && v.nr_columns == 2)
         if (v.matrix[0][0] == v.matrix[1][1] || v.matrix[0][1] == v.matrix[1][0]) {
-            if (verbose) std::cout << "* 2x2 diagonal matrix: check failed" << std::endl;
+            if (verbose) std::cout << "\033[1;31m[Fail: 2x2 diagonal matrix]\033[0m" << std::endl;
             return false;
         }
     // Number quantity check
@@ -76,7 +77,7 @@ bool preprocess(const struct vault& v) {
             numbers[v.matrix[row][column] - 1]++;
     for (int i = 0; i < v.nr_rows; i++)
         if (numbers[i] != v.nr_columns) {
-            std::cout << "* Number quantity: check failed" << std::endl;
+            std::cout << "\033[1;31m[Fail: number quantity]\033[0m" << std::endl;
             return false;
         }
 
@@ -88,52 +89,72 @@ bool preprocess(const struct vault& v) {
             else required += abs(v.matrix[i][j] - i) + 1;
         }
         if (required > v.max_moves) {
-            if (verbose) std::cout << "* Minimum amout of moves: check failed" << std::endl;
+            if (verbose) std::cout << "\033[1;31m[Fail: minimum amout of moves]\033[0m" << std::endl;
             return false;
         }
     }
     return true;
 }
 
-//TODO: Function to lock lines
-void lock() {}
+void lock(struct vault& v, std::vector<bool>& locks) {
+    // Top to bottom
+    for (int i = 0; i < v.nr_rows; i++) {
+        locks[i] = true;
+        for (int j = 0; j < v.nr_columns; j++)
+            if (v.matrix[i][j] != i + 1) {
+                locks[i] = false;
+                break; // Inner
+            }
+        if (!locks[i]) break; // Outer
+    }
+
+    // Bottom to top
+    for (int i = v.nr_rows - 1; i >= 0; i--) {
+        locks[i] = true;
+        for (int j = 0; j < v.nr_columns; j++)
+            if (v.matrix[i][j] != i + 1) {
+                locks[i] = false;
+                break; // Inner
+            }
+        if (!locks[i]) break; // Outer
+    }
+}
 
 int process(struct vault& v) {
-    if (v.best >= v.max_moves) return -1;
-    if (verify(v)) return v.max_moves;
-    if (v.max_moves == 0) return -1;
+    std::vector<bool> locks(v.nr_rows, false);
+    lock(v, locks);
 
-    int best_remaining = -1; // From all iterations
+    if (v.best >= v.max_moves || v.max_moves == 0) return -1;
+    if (verify(v)) return v.max_moves;
+
+    int best = -1;
 
     for (int i_row = 0; i_row < v.nr_rows - 1; i_row++) {
         for (int i_column = 0; i_column < v.nr_columns - 1; i_column++) {
-            int remaining; // From current iteration
+
+            // Locked rows
+            if (locks[i_row]) continue;
 
             // Rotate left
             rotate(v, i_column, i_row, LEFT, false);
-            remaining = process(v);
-            if (remaining > best_remaining) best_remaining = remaining;
+            best = std::max(process(v), best);
             rotate(v, i_column, i_row, RIGHT, true);
 
             // Rotate right
             rotate(v, i_column, i_row, RIGHT, false);
-            remaining = process(v);
-            if (remaining > best_remaining) best_remaining = remaining;
+            best = std::max(process(v), best);
             rotate(v, i_column, i_row, LEFT, true);
         }
     }
 
-    if (best_remaining > v.best)
-        v.best = best_remaining;
+    if (best > v.best)
+        v.best = best;
 
     return v.best;
 }
 
 int main(int argc, char const *argv[]) {
     if (argc > 1 && std::string(argv[1]) == "-d") verbose = true;
-
-    std::clock_t start = std::clock();
-    double duration;
 
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
@@ -142,17 +163,23 @@ int main(int argc, char const *argv[]) {
     std::cin >> nr_vaults;
 
     for (int i = 0; i < nr_vaults; i++) {
-        int answer = -1;
+        count = 0;
+        std::clock_t start = std::clock();
+        double duration;
+
+        int best = -1;
         struct vault v = read();
 
-        if (verify(v)) answer = v.max_moves;
-        else if (preprocess(v)) answer = process(v);
+        if (verify(v)) best = v.max_moves;
+        else if (preprocess(v)) best = process(v);
 
-        if (answer == -1) std::cout << "the treasure is lost!\n";
-        else std::cout << v.max_moves - answer << std::endl;
+        if (best == -1) std::cout << "the treasure is lost!\n";
+        else std::cout << v.max_moves - best << std::endl;
+
+        duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+        if (verbose) std::cout << "\033[1;32m[Chronometer: " << duration << " seconds]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[Count: " << count << "| Count locks: " << count_locks << "]\033[0m" << std::endl;
     }
 
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    if (verbose) std::cout << "* Chronometer: " << duration << " seconds" << std::endl;
     return 0;
 }
