7416d4f63ef69cf0b26a0a2e8d6bf2b0a97723da
tanhinho <miguel1leopoldo@gmail.com>
2024-03-14 12:37:30 +0000

    Code accepted and folder cleanups

--- problem_A/aztec_vaults.cpp
+++ problem_A/aztec_vaults.cpp
@@ -1,140 +1,88 @@
-/*
- * Read the problem A - "Aztec Vault" in EA2024_Trabalho1 at Mooshak (or aztec_vaults.md).
- */
-
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <string.h>
+#include <algorithm>
 
-// Stores information about a vault
+#include <thread> //! Debug
+
+#define LEFT 0
+#define RIGHT 1
+
+bool verbose = false;
+unsigned int count = 0;
+
 struct vault
 {
+    int best = -1;
+    int nr_rows{};
+    int nr_columns{};
+    int max_moves{};
+
     std::vector<std::vector<int>> matrix;
-    int max_moves = 0;
-    int nr_rows;
-    int nr_columns;
 };
 
-// Reads the input in the format given to store vault's information needed for the problem
-// Stores the information in the given matrix
-struct vault read_vault()
+struct vault read()
 {
-    struct vault vault;
-    std::cin >> vault.nr_rows >> vault.nr_columns >> vault.max_moves;
+    struct vault v;
+    std::cin >> v.nr_rows >> v.nr_columns >> v.max_moves;
 
-    // Declare the matrix to represent the vault with the given sizes
-    std::vector<std::vector<int>> vault_matrix(vault.nr_rows, std::vector<int>(vault.nr_columns));
+    std::vector<std::vector<int>> v_matrix(v.nr_rows, std::vector<int>(v.nr_columns));
 
-    // Initialize the vault with the values coming from the input
-    for (int i_row = 0; i_row < vault.nr_rows; i_row++)
-        for (int i_column = 0; i_column < vault.nr_columns; i_column++)
-            std::cin >> vault_matrix[i_row][i_column];
+    for (int i_row = 0; i_row < v.nr_rows; i_row++)
+        for (int i_column = 0; i_column < v.nr_columns; i_column++)
+            std::cin >> v_matrix[i_row][i_column];
 
-    vault.matrix = vault_matrix;
-    return vault;
+    v.matrix = v_matrix;
+    return v;
 }
 
-// Rotates the matrix in the given 2x2 block.
-// If rotate_right == false, rotate the 2x2 block left instead
-// "undo" is set to "true" if the call should be undoing a rotation and restoring the previous state
-void rotate_handle(struct vault *vault, int handle_x, int handle_y, bool rotate_right, bool undo)
+bool verify(const struct vault &v)
 {
-    int temp = vault->matrix[handle_y][handle_x];
-
-    if (rotate_right)
-    {
-        vault->matrix[handle_y][handle_x] = vault->matrix[handle_y + 1][handle_x];
-        vault->matrix[handle_y + 1][handle_x] = vault->matrix[handle_y + 1][handle_x + 1];
-        vault->matrix[handle_y + 1][handle_x + 1] = vault->matrix[handle_y][handle_x + 1];
-        vault->matrix[handle_y][handle_x + 1] = temp;
-    }
-    else
-    {
-        vault->matrix[handle_y][handle_x] = vault->matrix[handle_y][handle_x + 1];
-        vault->matrix[handle_y][handle_x + 1] = vault->matrix[handle_y + 1][handle_x + 1];
-        vault->matrix[handle_y + 1][handle_x + 1] = vault->matrix[handle_y + 1][handle_x];
-        vault->matrix[handle_y + 1][handle_x] = temp;
-    }
-
-    if (undo)
-        vault->max_moves++;
-    else
-        vault->max_moves--;
-}
-
-// Whether the vault is solved or not
-bool opened_vault(const struct vault vault)
-{
-    for (int i_row = 0; i_row < vault.nr_rows; i_row++)
-        for (int i_column = 0; i_column < vault.nr_columns; i_column++)
-            if (vault.matrix[i_row][i_column] != i_row + 1)
+    for (int i_row = 0; i_row < v.nr_rows; i_row++)
+        for (int i_column = 0; i_column < v.nr_columns; i_column++)
+            if (v.matrix[i_row][i_column] != i_row + 1)
                 return false;
 
     return true;
 }
-// Counts the given vault minimum rotations needed to solve it.
-// If the minimum rotations exceed the maximum, return -1
-int solve_vault_dfs(struct vault *vault, int *best_remaining_moves)
+
+void rotate(struct vault &v, int x, int y, bool side, bool undo)
 {
-    // Cut redundant cases by checking if the current state already has
-    // fewer remaining moves than the best remaining moves found
-    if (*best_remaining_moves >= vault->max_moves)
-        return -1;
+    count++;
+    int temp = v.matrix[y][x];
 
-    if (opened_vault(*vault))
-        return vault->max_moves;
-
-    if (vault->max_moves == 0)
-        return -1;
-
-    // The value below will always be equal or smaller than the first move returned from the recursion
-    int max_remaining_moves = -1;
-
-    // Create every possibility with the given vault and store
-    // the one that has the vault solved with the most number of moves remaining
-    for (int i_row = 0; i_row < vault->nr_rows - 1; i_row++)
+    // Rotate right
+    if (side)
     {
-        for (int i_column = 0; i_column < vault->nr_columns - 1; i_column++)
-        {
-            int remaining_moves;
-
-            // Rotate the vault right and see the maximum amount of remaining moves the resulting state will return
-            rotate_handle(vault, i_column, i_row, true, false);
-            remaining_moves = solve_vault_dfs(vault, best_remaining_moves);
-            if (remaining_moves > max_remaining_moves)
-                max_remaining_moves = remaining_moves;
-            // Restore the vault to the previous state
-            rotate_handle(vault, i_column, i_row, false, true);
-
-            // Rotate the vault left and see the maximum amount of remaining moves the resulting state will return
-            rotate_handle(vault, i_column, i_row, false, false);
-            remaining_moves = solve_vault_dfs(vault, best_remaining_moves);
-            if (remaining_moves > max_remaining_moves)
-                max_remaining_moves = remaining_moves;
-            // Restore the vault to the previous state
-            rotate_handle(vault, i_column, i_row, true, true);
-        }
+        v.matrix[y][x] = v.matrix[y + 1][x];
+        v.matrix[y + 1][x] = v.matrix[y + 1][x + 1];
+        v.matrix[y + 1][x + 1] = v.matrix[y][x + 1];
+        v.matrix[y][x + 1] = temp;
+    }
+    // Rotate left
+    else
+    {
+        v.matrix[y][x] = v.matrix[y][x + 1];
+        v.matrix[y][x + 1] = v.matrix[y + 1][x + 1];
+        v.matrix[y + 1][x + 1] = v.matrix[y + 1][x];
+        v.matrix[y + 1][x] = temp;
     }
 
-    // If the current solution has more remaining moves than the previous best, set it as the new best
-    if (max_remaining_moves > *best_remaining_moves)
-        *best_remaining_moves = max_remaining_moves;
-
-    return max_remaining_moves;
+    if (undo)
+        v.max_moves++;
+    else
+        v.max_moves--;
 }
 
-void lock_row(std::vector<int> *row)
-{
-}
-
-struct vault lock_solved_rows(const struct vault vault)
+// "Locks" rows by removing solved rows from the vault
+// Must be used after preprocessing or else errors may occur
+struct vault lock_rows(const struct vault vault)
 {
     struct vault locked_vault = vault;
     bool ignore_top_row = false;
     bool ignore_bot_row = false;
+    int top_rows_removed = 0;
 
-    // TODO: Analyze how the cycle will know when the top and bottom rows hit each other
     for (int i_row = 0; i_row < vault.nr_rows; i_row++)
     {
         bool lock_top_row = true;
@@ -142,114 +90,279 @@ struct vault lock_solved_rows(const struct vault vault)
 
         for (int i_column = 0; i_column < vault.nr_columns; i_column++)
         {
-            locked_vault.matrix[i_row][i_column]--;
-            locked_vault.matrix[vault.nr_rows - i_row - 1][vault.nr_columns - i_column - 1]--;
-
-            // In both the below cases, we will be checking if the current row is solved. If it is, mark them as needing to be locked.
-            // This will be treated later
+            // In both the below cases, we will be checking if the current row is solved
+            // If it isn't, mark them as not needing to be locked
 
             // First row going down each iteration
             if (vault.matrix[i_row][i_column] != i_row + 1)
                 lock_top_row = false;
             // Last row going up each iteration
-            if (vault.matrix[vault.nr_rows - i_row - 1][vault.nr_columns - i_column - 1] != vault.nr_rows - i_row)
+            if (vault.matrix[vault.nr_rows - i_row - 1][i_column] != vault.nr_rows - i_row)
                 lock_bot_row = false;
         }
 
         // Treat the rows that need to be locked
         // If the top row was solved, remove it from the matrix
         if (!ignore_top_row && lock_top_row)
+        {
             locked_vault.matrix.erase(locked_vault.matrix.begin());
+            locked_vault.nr_rows--;
+            top_rows_removed++;
+        }
         // Else, set to ignore the next rows coming from the top, because rows cant be locked if the rows above them are not solved
         else
             ignore_top_row = true;
 
         // If the bottom row was solved, remove it from the matrix
         if (!ignore_bot_row && lock_bot_row)
+        {
             locked_vault.matrix.pop_back();
+            locked_vault.nr_rows--;
+        }
         // Else, set to ignore the next rows coming from the bottom, because rows cant be locked if the rows below them are not solved
         else
-            ignore_top_row = true;
+            ignore_bot_row = true;
 
         // If both top and bottom rows are set to be ignored, no need going through the cycle again
         if (ignore_bot_row && ignore_top_row)
             break;
     }
 
-    // TODO: Decrement every value in the matrix for each top row removed
+    for (int i_row = 0; i_row < locked_vault.nr_rows; i_row++)
+        for (int i_column = 0; i_column < locked_vault.nr_columns; i_column++)
+            locked_vault.matrix[i_row][i_column] -= top_rows_removed;
+
+    return locked_vault;
 }
 
-// Count the occurrence of a pattern, which is represented by an integer, within a given vault
-int count_pattern_occurrence(const struct vault vault, int pattern)
+struct vault rotate_vault(const struct vault vault, bool side)
 {
-    int pattern_occurrence = 0;
+    struct vault rotated_vault;
+    rotated_vault.matrix = std::vector<std::vector<int>>(vault.nr_columns, std::vector<int>(vault.nr_rows));
 
-    for (int i_row = 0; i_row < vault.nr_rows; i_row++)
-        for (int i_column = 0; i_column < vault.nr_columns; i_column++)
-            pattern_occurrence += vault.matrix[i_row][i_column] == pattern;
+    if (side == LEFT)
+    {
+        for (int i_row = vault.nr_rows - 1; i_row >= 0; i_row--)
+            for (int i_column = 0; i_column < vault.nr_columns; i_column++)
+                rotated_vault.matrix[i_column][i_row] = vault.matrix[i_row][i_column];
+        // After transposing the vault, the rows are reversed, so reverse them to be in the order needed
+        std::reverse(rotated_vault.matrix.begin(), rotated_vault.matrix.end());
+    }
+    else
+    {
+        for (int i_row = 0; i_row < vault.nr_rows; i_row++)
+            for (int i_column = 0; i_column < vault.nr_columns; i_column++)
+                rotated_vault.matrix[i_column][i_row] = vault.matrix[i_row][i_column];
+        // After transposing the vault, the columns are reversed, so reverse them to be in the order needed
+        for (std::vector<int> &row : rotated_vault.matrix)
+            std::reverse(row.begin(), row.end());
+    }
 
-    return pattern_occurrence;
+    rotated_vault.nr_rows = vault.nr_columns;
+    rotated_vault.nr_columns = vault.nr_rows;
+    rotated_vault.max_moves = vault.max_moves;
+
+    return rotated_vault;
 }
 
-// Whether the vault is solvable or not
-bool solvable_vault(const struct vault vault)
+struct vault lock_columns(const struct vault vault)
 {
-    // Verify if it's a 2x2 diagonal matrix
-    if (vault.nr_rows == 2 && vault.nr_columns == 2)
-        if (vault.matrix[0][0] == vault.matrix[1][1] || vault.matrix[0][1] == vault.matrix[1][0])
-            return false;
+    // Rotate the vault so the removal of the columns will be easier, since they are now rows
+    struct vault locked_vault = rotate_vault(vault, LEFT);
+    bool ignore_left_column = false;
+    bool ignore_right_column = false;
 
-    // Verify if the patterns are possible to be completed
-    for (int pattern = 1; pattern < vault.nr_rows + 1; pattern++)
-        if (count_pattern_occurrence(vault, pattern) != vault.nr_columns)
+    for (int i_column = 0; i_column < vault.nr_columns; i_column++)
+    {
+        bool lock_left_column = true;
+        bool lock_right_column = true;
+
+        for (int i_row = 0; i_row < vault.nr_rows; i_row++)
+        {
+            // In both the below cases, we will be checking if the current column is solved.
+            // If it isn't, mark them as not needing to be locked
+
+            // Left column going right each iteration
+            if (vault.matrix[i_row][i_column] != i_row + 1)
+                lock_left_column = false;
+            // Right column going left each iteration
+            if (vault.matrix[i_row][vault.nr_columns - i_column - 1] != i_row + 1)
+                lock_right_column = false;
+        }
+        // Treat the columns that need to be locked
+        // If the left column was solved, remove it from the matrix
+        if (!ignore_left_column && lock_left_column)
+        {
+            // The left column is removed by erasing the bottom row in the matrix because it was rotated
+            locked_vault.matrix.pop_back();
+            // The number of rows are decreased and not columns because when the rotation is undone,
+            // the nr of rows becomes the nr of columns, and vice-versa
+            locked_vault.nr_rows--;
+        }
+        // Else, set to ignore the next columns coming from the left,
+        // because left columns can't be locked if the column left of them are not solved
+        else
+            ignore_left_column = true;
+
+        // If the nr of rows (which will be the nr of columns) are 3 because a lower nr can lead to a matrix that can't be solved
+        if (locked_vault.nr_rows == 3)
+            break;
+
+        // If the right column was solved, remove it from the matrix
+        if (!ignore_right_column && lock_right_column)
+        {
+            // The right column is removed by erasing the top row in the matrix because it was rotated
+            locked_vault.matrix.erase(locked_vault.matrix.begin());
+            // The number of rows are decreased and not columns because when the rotation is undone,
+            // the nr of rows becomes the nr of columns, and vice-versa
+            locked_vault.nr_rows--;
+        }
+        // Else, set to ignore the next columns coming from the right,
+        // because right columns can't be locked if the column right of them are not solved
+        else
+            ignore_right_column = true;
+
+        // If both left and right columns are set to be ignored, no need going through the cycle again
+        // Or if the nr of rows (which will be the nr of columns) are 3 because a lower nr can lead to a matrix that can't be solved
+        if (ignore_right_column && ignore_left_column || locked_vault.nr_rows == 3)
+            break;
+    }
+
+    return rotate_vault(locked_vault, RIGHT);
+}
+
+bool preprocess(const struct vault &v)
+{
+    // 2x2 diagonal matrix check
+    if (v.nr_rows == 2 && v.nr_columns == 2)
+        if (v.matrix[0][0] == v.matrix[1][1] || v.matrix[0][1] == v.matrix[1][0])
+        {
+            if (verbose)
+                std::cout << "\033[1;31m[Fail: 2x2 diagonal matrix]\033[0m" << std::endl;
             return false;
+        }
+    // Number quantity check
+    std::vector<int> numbers(v.nr_rows);
+    for (int row = 0; row < v.nr_rows; row++)
+        for (int column = 0; column < v.nr_columns; column++)
+            numbers[v.matrix[row][column] - 1]++;
+    for (int i = 0; i < v.nr_rows; i++)
+        if (numbers[i] != v.nr_columns)
+        {
+            if (verbose)
+                std::cout << "\033[1;31m[Fail: number quantity]\033[0m" << std::endl;
+            return false;
+        }
 
     return true;
 }
 
-// Begins the recursive process of solving a vault
-int solve_vault(struct vault vault)
+bool has_min_moves(const struct vault v)
 {
-    // Despite checking if it is open after the recursion begins,
-    // checking here prevents errors when locking rows
-    if (opened_vault(vault))
-        return vault.max_moves;
+    for (int i = 0; i < v.nr_rows; i++)
+    {
+        int required = 0;
+        for (int j = 0; j < v.nr_columns; j++)
+        {
+            if (v.matrix[i][j] > i)
+                required += abs(v.matrix[i][j] - i) - 1;
+            else
+                required += abs(v.matrix[i][j] - i) + 1;
+        }
+        if (required > v.max_moves)
+        {
+            if (verbose)
+                std::cout << "* Minimum amout of moves: check failed" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
 
-    if (!solvable_vault(vault))
+int process(struct vault &v)
+{
+    if (v.best >= v.max_moves)
+        return -1;
+    if (verify(v))
+        return v.max_moves;
+    if (v.max_moves == 0)
+        return -1;
+    if (!has_min_moves(v))
         return -1;
 
-    int best_remaining_moves = -1;
-    solve_vault_dfs(&vault, &best_remaining_moves);
-    return best_remaining_moves;
+    int best = -1;
+
+    for (int i_row = 0; i_row < v.nr_rows - 1; i_row++)
+    {
+        for (int i_column = 0; i_column < v.nr_columns - 1; i_column++)
+        {
+            // Rotate left
+            rotate(v, i_column, i_row, LEFT, false);
+            best = std::max(process(v), best);
+            rotate(v, i_column, i_row, RIGHT, true);
+
+            // Rotate right
+            rotate(v, i_column, i_row, RIGHT, false);
+            best = std::max(process(v), best);
+            rotate(v, i_column, i_row, LEFT, true);
+        }
+    }
+
+    if (best > v.best)
+        v.best = best;
+
+    return v.best;
 }
 
 int main(int argc, char const *argv[])
 {
-    // Debug
-    std::clock_t start = std::clock();
-    double duration;
+    if (argc > 1 && std::string(argv[1]) == "-d")
+        verbose = true;
 
-    // Makes IO faster
+    std::clock_t total_time = std::clock();
+
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int nr_test_cases;
-    std::cin >> nr_test_cases;
+    int nr_vaults;
+    std::cin >> nr_vaults;
 
-    for (int i = 0; i < nr_test_cases; i++)
+    for (int i = 0; i < nr_vaults; i++)
     {
-        struct vault vault = read_vault();
-        int remaining_moves = solve_vault(vault);
+        count = 0;
+        double duration;
+        std::clock_t case_time = std::clock();
 
-        if (remaining_moves == -1)
+        int best = -1;
+        struct vault v = read();
+
+        if (verify(v))
+            best = v.max_moves;
+        else if (preprocess(v))
+        {
+            v = lock_rows(v);
+            // v = lock_columns(v);
+            best = process(v);
+        }
+
+        if (best == -1)
             std::cout << "the treasure is lost!\n";
         else
-            std::cout << vault.max_moves - remaining_moves << std::endl;
+            std::cout << v.max_moves - best << std::endl;
+
+        duration = (std::clock() - case_time) / (double)CLOCKS_PER_SEC;
+        if (verbose)
+        {
+            std::cout << "\033[1;32m[Chronometer: " << duration << " seconds]\033[0m" << std::endl;
+            std::cout << "\033[1;32m[Rotation count: " << count << "]\033[0m" << std::endl;
+        }
     }
 
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    if (argc == 2 && strcmp(argv[1], "-d") == 0)
-        std::cout << "Took " << duration << " seconds" << std::endl;
+    if (verbose)
+    {
+        double total_duration = (std::clock() - total_time) / (double)CLOCKS_PER_SEC;
+        std::cout << "\033[1;33m[Total time: " << total_duration << " seconds]\033[0m" << std::endl;
+    }
 
     return 0;
 }
