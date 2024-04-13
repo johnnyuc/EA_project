a0b621609f6db274a910214d975a9dda34c60955
tanhinho <miguel1leopoldo@gmail.com>
2024-04-13 15:52:02 +0100

    Solving with permutations working

--- problem_B/aztec_cards2.cpp
+++ problem_B/aztec_cards2.cpp
@@ -16,58 +16,69 @@ struct card_grid {
     int cards_per_row{};
     int cards_per_column{};
 
-    std::vector<int> rows_left{};
     std::vector<int> columns_left{};
+
+    std::vector<std::vector<bool>> permutations{};
 
     std::vector<std::vector<bool>> matrix;
 };
+
+std::vector<std::vector<bool>> generatePermutations(int numTrue, int size) {
+    // Generate the initial permutation with 'numTrue' true values followed by false values
+    std::vector<bool> initial_permutation(size);
+    fill_n(initial_permutation.begin(), numTrue, true);
+
+    // Vector to store all permutations, including the initial permutation
+    std::vector<std::vector<bool>> permutations{initial_permutation};
+
+    // Generate all permutations
+    while (prev_permutation(initial_permutation.begin(), initial_permutation.end()))
+        permutations.push_back(initial_permutation);
+
+    return permutations;
+}
 
 // Reads the input and returns a card grid structure
 struct card_grid read_input() {
     struct card_grid card_grid;
     std::cin >> card_grid.nr_columns >> card_grid.nr_rows;
-    std::cin >> card_grid.cards_per_row >> card_grid.cards_per_column;
-
-    card_grid.rows_left = std::vector<int>(card_grid.nr_rows, card_grid.cards_per_row);
+    std::cin >> card_grid.cards_per_column >> card_grid.cards_per_row;
 
     card_grid.columns_left = std::vector<int>(card_grid.nr_columns, card_grid.cards_per_column);
 
     // Initialize the card grid matrix with zeros
     card_grid.matrix = std::vector<std::vector<bool>>(card_grid.nr_rows, std::vector<bool>(card_grid.nr_columns, 0));
 
+    // Initialize the permutations vector
+    card_grid.permutations = generatePermutations(card_grid.cards_per_row, card_grid.nr_columns);
+
     return card_grid;
 }
 
-static bool grid_solved(struct card_grid grid) {
-    // Calculate the sum of grid.rows_left and grid.columns_left
+static bool grid_solved(struct card_grid grid, int row) {
+    // Calculate the sum of grid.columns_left
     int sum = 0;
-    for (int i = 0; i < grid.nr_rows; i++)
-        sum += grid.rows_left[i];
-
     for (int i = 0; i < grid.nr_columns; i++)
         sum += grid.columns_left[i];
 
-    return sum == 0;
-}
-
-static bool has_card(struct card_grid grid, int row, int column) {
-    return grid.matrix[row][column];
+    return sum == 0 && row == grid.nr_rows;
 }
 
 static bool reached_invalid_row(struct card_grid grid, int row) {
     return row == grid.nr_rows;
 }
 
-void print_vector(std::vector<int> v) {
+static void print_vector(std::vector<int> v) {
     for (int i = 0; i < v.size(); i++)
         std::cout << v[i] << " ";
     std::cout << std::endl;
 }
 
-void print_grid(struct card_grid grid) {
+static void print_grid(struct card_grid grid) {
+    print_vector(grid.columns_left);
     for (int i = 0; i < grid.nr_rows; i++) {
         for (int j = 0; j < grid.nr_columns; j++) {
-            if (has_card(grid, i, j))
+            if (grid.matrix[i][j])
                 std::cout << "X ";
             else
                 std::cout << "- ";
@@ -77,26 +88,31 @@ void print_grid(struct card_grid grid) {
     std::cout << std::endl;
 }
 
-void place_first_cards_row(struct card_grid &grid, int row) {
-    for (int i = 0; i < grid.cards_per_column; i++) {
-        grid.matrix[row][i] = true;
-        grid.columns_left[i]--;
+static bool impossible_grid(struct card_grid grid) {
+    for (int i = 0; i < grid.nr_columns; i++) {
+        if (grid.columns_left[i] < 0)
+            return true;
     }
 
-    grid.rows_left[row] = 0;
+    return false;
 }
 
-void remove_cards_row(struct card_grid &grid, int row) {
-    // Assuming that this function is only called with the last permutation of the row
-    for (int i = grid.nr_columns - grid.cards_per_row; i < grid.nr_columns; i++) {
-        grid.matrix[row][i] = false;
-        grid.columns_left[i]++;
+static std::vector<int> calculate_columns_left(struct card_grid grid, int row) {
+    std::vector<int> columns_left = grid.columns_left;
+    for (int i = 0; i < grid.nr_columns; i++) {
+        if (grid.matrix[row][i])
+            columns_left[i]--;
     }
+
+    return columns_left;
 }
 
-void process(struct card_grid grid, int row, int &solutions) {
-    print_grid(grid);
-    if (grid_solved(grid)) {
+void process(struct card_grid grid, int row, long &solutions) {
+    // print_grid(grid);
+    if (impossible_grid(grid))
+        return;
+
+    if (grid_solved(grid, row)) {
         solutions++;
         return;
     }
@@ -104,16 +120,15 @@ void process(struct card_grid grid, int row, int &solutions) {
     if (reached_invalid_row(grid, row))
         return;
 
-    // Place the first cards of the current row
-    place_first_cards_row(grid, row);
-
-    do {
+    // For each permutation
+    std::vector<int> save_columns_left = grid.columns_left;
+    for (int i = 0; i < grid.permutations.size(); i++) {
+        grid.matrix[row] = grid.permutations[i];
+        grid.columns_left = calculate_columns_left(grid, row);
         process(grid, row + 1, solutions);
-        // Generate all possible permutations of the current row
-    } while (prev_permutation(grid.matrix[row].begin(), grid.matrix[row].end()));
-
-    // When all permutations are done, remove the cards from the current row
-    remove_cards_row(grid, row);
+        grid.columns_left = save_columns_left;
+    }
+    grid.matrix[row] = std::vector<bool>(grid.nr_columns, false);
 
     return;
 }
@@ -123,8 +138,9 @@ int main() {
     std::cin >> nr_tests;
 
     for (int i = 0; i < nr_tests; i++) {
-        int solutions = 0;
-        process(read_input(), 0, solutions);
+        struct card_grid grid = read_input();
+        long solutions = 0;
+        process(grid, 0, solutions);
         std::cout << solutions << std::endl;
     }
     return 0;
