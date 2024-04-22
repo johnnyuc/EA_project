b62624d772d0aaa41495a8db8dd179701f11d6f4
Johnny Fernandes <alfernandes@student.dei.uc.pt>
2024-04-22 15:58:25 +0100

    Preparing problem_C

--- problem_C/aztec_lab.cpp
+++ problem_C/aztec_lab.cpp
@@ -6,4 +6,12 @@
  * Algorithmic Strategies 2023/24
  */
 
+/*
+ * Stuff to base the solution on:
+ * https://cses.fi/problemset/task/1192
+ * https://cses.fi/problemset/task/1193
+ * https://cses.fi/problemset/task/1194
+ * https://github.com/mrsac7/CSES-Solutions/blob/master/src/1194%20-%20Monsters.cpp
+ */
+
 #include <iostream>
